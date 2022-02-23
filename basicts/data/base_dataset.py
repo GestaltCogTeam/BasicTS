17a5f1e03f17eedd901b8cf63249dcad635d62ee
Zezhi Shao <864453277@qq.com>
2022-02-23 11:47:11 +0800

    add base dataset

---
+++ basicts/data/base_dataset.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+from utils.serialization import load_pkl
+
+import torch
+from torch.utils.data import Dataset
+
+class BaseDataset(Dataset):
+    def __init__(self, raw_file_path, index_file_path, mode, **kwargs) -> None:
+        super().__init__()
+        assert mode in ['train', 'valid', 'test'], "error mode"
+        # read raw data (normalized)
+        data  = load_pkl(raw_file_path)
+        raw_data = data['raw_data']
+        self.data = torch.from_numpy(raw_data).float()               # L, N, C
+        # read index
+        self.index = load_pkl(index_file_path)[mode]
+
+    def __getitem__(self, index):
+        """get a sample.
+
+        Args:
+            index (tuple): the iteration index (not the self.index)
+
+        Returns:
+            tuple: (future_data, history_data), where the shape of each is L x N x C.
+        """
+        idx = list(self.index[index])
+        idx[0] = [1,2,3,4,5,6,7,8,9,11]
+        if isinstance(idx[0], int):
+            # continuous index
+            history_data  = self.data[idx[0]:idx[1]]
+            future_data   = self.data[idx[1]:idx[2]]
+        else:
+            # discontinuous index or custom index
+            # NOTE: current time $t$ should not included in the index[0]
+            history_index = idx[0]    # list
+            assert idx[1] not in history_index, "current time t should not included in the idx[0]"
+            history_index.append(idx[1])
+            history_data = self.data[history_index]
+            future_data = self.data[idx[1], idx[2]]
+
+        return future_data, history_data
+        
+    def __len__(self):
+        """dataset length
+
+        Returns:
+            int: dataset length
+        """
+        return len(self.index)
