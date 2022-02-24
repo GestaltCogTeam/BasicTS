9a01798fe6655de01fa906744fd68df9788cb690
Zezhi Shao <864453277@qq.com>
2022-02-24 11:37:52 +0800

    feature: add Graph WaveNet

---
+++ basicts/options/GraphWaveNet/GraphWaveNet_PEMS04.py
@@ -0,0 +1,118 @@
+import os
+from easydict import EasyDict
+import torch
+# architecture 
+from basicts.archs.GraphWaveNet_arch import GraphWaveNet
+# runner
+from basicts.runners.GraphWaveNet_runner import GraphWaveNetRunner
+from basicts.data.base_dataset import BaseDataset
+from basicts.metrics.mae import masked_mae
+from basicts.metrics.mape import masked_mape
+from basicts.metrics.rmse import masked_rmse
+from basicts.losses.losses import l1_loss
+from basicts.utils.serialization import load_adj
+
+CFG = EasyDict()
+
+# ================= general ================= #
+CFG.DESCRIPTION = 'Graph WaveNet model configuration'
+CFG.RUNNER  = GraphWaveNetRunner
+CFG.DATASET_CLS   = BaseDataset
+CFG.DATASET_NAME  = "PEMS04"
+CFG.DATASET_TYPE  = 'Traffic flow'
+CFG.GPU_NUM = 1
+CFG.SEED    = 1
+CFG.CUDNN_ENABLED = True
+CFG.METRICS = {
+    "MAE": masked_mae,
+    "MAPE": masked_mape,
+    "RMSE": masked_rmse
+}
+
+# ================= model ================= #
+CFG.MODEL = EasyDict()
+CFG.MODEL.NAME  = 'Graph WaveNet'
+CFG.MODEL.ARCH  = GraphWaveNet
+adj_mx, _ = load_adj("datasets/" + CFG.DATASET_NAME + "/adj_mx.pkl", "doubletransition")
+CFG.MODEL.PARAM = {
+    "num_nodes" : 307, 
+    "supports"  :[torch.tensor(i) for i in adj_mx],
+    "dropout"   : 0.3, 
+    "gcn_bool"  : True, 
+    "addaptadj" : True, 
+    "aptinit"   : None, 
+    "in_dim"    : 2,
+    "out_dim"   : 12,
+    "residual_channels" : 32,
+    "dilation_channels" : 32,
+    "skip_channels"     : 256,
+    "end_channels"      : 512,
+    "kernel_size"       : 2,
+    "blocks"            : 4,
+    "layers"            : 2
+}
+CFG.MODEL.FROWARD_FEATURES = [0, 1]            # traffic speed, time in day
+CFG.MODEL.TARGET_FEATURES  = [0]                # traffic speed
+
+# ================= optim ================= #
+CFG.TRAIN = EasyDict()
+CFG.TRAIN.LOSS = l1_loss
+CFG.TRAIN.OPTIM = EasyDict()
+CFG.TRAIN.OPTIM.TYPE = "Adam"
+CFG.TRAIN.OPTIM.PARAM= {
+    "lr":0.005,
+    "weight_decay":1.0e-5,
+}
+CFG.TRAIN.LR_SCHEDULER = EasyDict()
+CFG.TRAIN.LR_SCHEDULER.TYPE = "MultiStepLR"
+CFG.TRAIN.LR_SCHEDULER.PARAM= {
+    "milestones":[50],
+    "gamma":0.1
+}
+
+# ================= train ================= #
+CFG.TRAIN.CLIP       = 5
+CFG.TRAIN.NUM_EPOCHS = 100
+CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+    'checkpoints',
+    '_'.join([CFG.MODEL.NAME, str(CFG.TRAIN.NUM_EPOCHS)])
+)
+# train data
+CFG.TRAIN.DATA          = EasyDict()
+CFG.TRAIN.NULL_VAL      = 0.0
+## read data
+CFG.TRAIN.DATA.DIR      = 'datasets/' + CFG.DATASET_NAME
+## dataloader args, optional
+CFG.TRAIN.DATA.BATCH_SIZE   = 64
+CFG.TRAIN.DATA.PREFETCH     = False
+CFG.TRAIN.DATA.SHUFFLE      = True
+CFG.TRAIN.DATA.NUM_WORKERS  = 2
+CFG.TRAIN.DATA.PIN_MEMORY   = False
+
+# ================= validate ================= #
+CFG.VAL = EasyDict()
+CFG.VAL.INTERVAL = 1
+# validating data
+CFG.VAL.DATA = EasyDict()
+## read data
+CFG.VAL.DATA.DIR      = 'datasets/' + CFG.DATASET_NAME
+## dataloader args, optional
+CFG.VAL.DATA.BATCH_SIZE     = 64
+CFG.VAL.DATA.PREFETCH       = False
+CFG.VAL.DATA.SHUFFLE        = True
+CFG.VAL.DATA.NUM_WORKERS    = 2
+CFG.VAL.DATA.PIN_MEMORY     = False
+
+# ================= test ================= #
+CFG.TEST = EasyDict()
+CFG.TEST.INTERVAL = 1
+# validating data
+CFG.TEST.DATA = EasyDict()
+## read data
+CFG.TEST.DATA.DIR      = 'datasets/' + CFG.DATASET_NAME
+## dataloader args, optional
+CFG.TEST.DATA.BATCH_SIZE    = 64
+CFG.TEST.DATA.PREFETCH      = False
+CFG.TEST.DATA.SHUFFLE       = True
+CFG.TEST.DATA.NUM_WORKERS   = 2
+CFG.TEST.DATA.PIN_MEMORY    = False
--- basicts/run.py
+++ basicts/run.py
@@ -9,9 +9,10 @@ setproctitle.setproctitle("STEP")
 
 def parse_args():
     parser = ArgumentParser(description='Run time series forecasting model in BasicTS framework based on EasyTorch!')
-    # parser.add_argument('-opt', '--cfg', required=True, help='training config')
+    parser.add_argument('-opt', '--cfg', required=True, help='training config')
     # parser.add_argument('-opt', '--cfg', default='basicts/options/HI/HI_METR-LA.py', help='training config')
-    parser.add_argument('-opt', '--cfg', default='basicts/options/GraphWaveNet/GraphWaveNet_METR-LA.py', help='training config')
+    # parser.add_argument('-opt', '--cfg', default='basicts/options/GraphWaveNet/GraphWaveNet_METR-LA.py', help='training config')
+    # parser.add_argument('-opt', '--cfg', default='basicts/options/GraphWaveNet/GraphWaveNet_PEMS04.py', help='training config')
     parser.add_argument('--gpus', default=None, help='visible gpus')
     return parser.parse_args()
 
