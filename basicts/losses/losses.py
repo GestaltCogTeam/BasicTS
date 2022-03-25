723e5e7ea7724e50eca2e0d4556859ccd709b579
Zezhi Shao <864453277@qq.com>
2022-03-25 16:45:21 +0800

    update AGCRN options

--- basicts/losses/losses.py
+++ basicts/losses/losses.py
@@ -1,1 +1,7 @@
 from basicts.metrics.mae import masked_mae as maksed_l1_loss
+
+import torch
+import torch.nn.functional as F
+
+def L1Loss(input, target, **kwargs):
+    return F.l1_loss(input, target)
