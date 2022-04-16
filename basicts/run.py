6097722fd2300299b515bdde4018220923ac1914
Zezhi Shao <864453277@qq.com>
2022-04-16 13:02:59 +0800

    fix bugs

--- basicts/run.py
+++ basicts/run.py
@@ -6,7 +6,7 @@ from easytorch.easytorch import launch_training
 
 def parse_args():
     parser = ArgumentParser(description='Run time series forecasting model in BasicTS framework based on EasyTorch!')
-    # parser.add_argument('-opt', '--cfg', required=True, help='training config')
+    parser.add_argument('-opt', '--cfg', required=True, help='training config')
     # parser.add_argument('-opt', '--cfg', default='basicts/options/HI/HI_METR-LA.py', help='training config')
     # parser.add_argument('-opt', '--cfg', default='basicts/options/HI/HI_PEMS-BAY.py', help='training config')
     # parser.add_argument('-opt', '--cfg', default='basicts/options/HI/HI_PEMS04.py', help='training config')
