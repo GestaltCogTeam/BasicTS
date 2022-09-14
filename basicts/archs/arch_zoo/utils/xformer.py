1d84a54e526fb7507aadf25827c9fb355a087324
Zezhi Shao <864453277@qq.com>
2022-09-14 15:52:43 +0800

    fix: 🐛 start_token_len = 0

--- basicts/archs/arch_zoo/utils/xformer.py
+++ basicts/archs/arch_zoo/utils/xformer.py
@@ -22,12 +22,17 @@ def data_transformation_4_xformer(history_data: torch.Tensor, future_data: torch
     x_mark_enc = history_data[:, :, 0, 1:]    # B, L1, C-1
 
     # get the x_dec
-    x_dec_token = x_enc[:, -start_token_len:, :]            # B, start_token_length, N
-    x_dec_zeros = torch.zeros_like(future_data[..., 0])     # B, L2, N
-    x_dec = torch.cat([x_dec_token, x_dec_zeros], dim=1)    # B, (start_token_length+L2), N
-    # get the corresponding x_mark_dec
-    x_mark_dec_token = x_mark_enc[:, -start_token_len:, :]            # B, start_token_length, C-1
-    x_mark_dec_future = future_data[..., :, 0, 1:]          # B, L2, C-1
-    x_mark_dec = torch.cat([x_mark_dec_token, x_mark_dec_future], dim=1)    # B, (start_token_length+L2), C-1
+    if start_token_len == 0:
+        x_dec = torch.zeros_like(future_data[..., 0])     # B, L2, N
+        x_mark_dec = future_data[..., :, 0, 1:]                 # B, L2, C-1
+        return x_enc, x_mark_enc, x_dec, x_mark_dec
+    else:
+        x_dec_token = x_enc[:, -start_token_len:, :]            # B, start_token_length, N
+        x_dec_zeros = torch.zeros_like(future_data[..., 0])     # B, L2, N
+        x_dec = torch.cat([x_dec_token, x_dec_zeros], dim=1)    # B, (start_token_length+L2), N
+        # get the corresponding x_mark_dec
+        x_mark_dec_token = x_mark_enc[:, -start_token_len:, :]            # B, start_token_length, C-1
+        x_mark_dec_future = future_data[..., :, 0, 1:]          # B, L2, C-1
+        x_mark_dec = torch.cat([x_mark_dec_token, x_mark_dec_future], dim=1)    # B, (start_token_length+L2), C-1 
 
     return x_enc, x_mark_enc, x_dec, x_mark_dec
