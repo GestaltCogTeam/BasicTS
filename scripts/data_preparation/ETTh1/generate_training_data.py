6f5edc34b5fbdc02db364abceb92e62a04d9c7d0
Zezhi Shao <864453277@qq.com>
2022-11-08 00:15:38 +0800

    add ETT datasets

---
+++ scripts/data_preparation/ETTh1/generate_training_data.py
@@ -0,0 +1,173 @@
+import os
+import sys
+import shutil
+import pickle
+import argparse
+
+import numpy as np
+import pandas as pd
+
+# TODO: remove it when basicts can be installed by pip
+sys.path.append(os.path.abspath(__file__ + "/../../../.."))
+from basicts.data.transform import standard_transform
+
+
+def generate_data(args: argparse.Namespace):
+    """Preprocess and generate train/valid/test datasets.
+    Default settings of METR-LA dataset:
+        - Normalization method: standard norm.
+        - Dataset division: 7:1:2.
+        - Window size: history 12, future 12.
+        - Channels (features): three channels [traffic speed, time of day, day of week]
+        - Target: predict the traffic speed of the future 12 time steps.
+
+    Args:
+        args (argparse): configurations of preprocessing
+    """
+
+    target_channel = args.target_channel
+    future_seq_len = args.future_seq_len
+    history_seq_len = args.history_seq_len
+    add_time_of_day = args.tod
+    add_day_of_week = args.dow
+    add_day_of_month = args.dom
+    add_day_of_year = args.doy
+    output_dir = args.output_dir
+    train_ratio = args.train_ratio
+    valid_ratio = args.valid_ratio
+    data_file_path = args.data_file_path
+
+    # read data
+    df = pd.read_csv(data_file_path)
+    df_index = pd.to_datetime(df['date'].values, format='%Y-%m-%d %H:%M').to_numpy()
+    df = df[df.columns[1:]]
+    df.index = df_index
+
+    data = np.expand_dims(df.values, axis=-1)
+
+    data = data[..., target_channel]
+    print("raw time series shape: {0}".format(data.shape))
+
+    l, n, f = data.shape
+    num_samples = l - (history_seq_len + future_seq_len) + 1
+    train_num_short = round(num_samples * train_ratio)
+    valid_num_short = round(num_samples * valid_ratio)
+    test_num_short = num_samples - train_num_short - valid_num_short
+    print("number of training samples:{0}".format(train_num_short))
+    print("number of validation samples:{0}".format(valid_num_short))
+    print("number of test samples:{0}".format(test_num_short))
+
+    index_list = []
+    for t in range(history_seq_len, num_samples + history_seq_len):
+        index = (t-history_seq_len, t, t+future_seq_len)
+        index_list.append(index)
+
+    train_index = index_list[:train_num_short]
+    valid_index = index_list[train_num_short: train_num_short + valid_num_short]
+    test_index = index_list[train_num_short +
+                            valid_num_short: train_num_short + valid_num_short + test_num_short]
+
+    scaler = standard_transform
+    data_norm = scaler(data, output_dir, train_index, history_seq_len, future_seq_len, heterogeneous=True)      # ETT series of datasets are heterogeneous between the different time series.
+
+    # add external feature
+    feature_list = [data_norm]
+    if add_time_of_day:
+        # IMPORTANT NOTE: 
+        # # In traffic related datasets (e.g., METR-LA, PEMS-BAY, PEMS0X) which are usually used in spatial-temporal prediciton in STGNN-based methods, the time of day is normalized to [0, 1].
+        # # However, in other datasets (e.g., ETT) which are usually used in Long Time Series Forecasting (LSTF) in Transformer-based methods, the time of day is not normalized.
+        tod = df.index.hour
+        tod_tiled = np.tile(tod, [1, n, 1]).transpose((2, 1, 0))
+        feature_list.append(tod_tiled)
+
+    if add_day_of_week:
+        # numerical day_of_week
+        dow = df.index.dayofweek
+        dow_tiled = np.tile(dow, [1, n, 1]).transpose((2, 1, 0))
+        feature_list.append(dow_tiled)
+
+    if add_day_of_month:
+        # numerical day_of_month
+        dom = df.index.day - 1 # df.index.day starts from 1. We need to minus 1 to make it start from 0.
+        dom_tiled = np.tile(dom, [1, n, 1]).transpose((2, 1, 0))
+        feature_list.append(dom_tiled)
+
+    if add_day_of_year:
+        # numerical day_of_year
+        doy = df.index.dayofyear - 1 # df.index.month starts from 1. We need to minus 1 to make it start from 0.
+        doy_tiled = np.tile(doy, [1, n, 1]).transpose((2, 1, 0))
+        feature_list.append(doy_tiled)
+
+    processed_data = np.concatenate(feature_list, axis=-1)
+
+    # dump data
+    index = {}
+    index["train"] = train_index
+    index["valid"] = valid_index
+    index["test"] = test_index
+    with open(output_dir + "/index_in{0}_out{1}.pkl".format(history_seq_len, future_seq_len), "wb") as f:
+        pickle.dump(index, f)
+
+    data = {}
+    data["processed_data"] = processed_data
+    with open(output_dir + "/data_in{0}_out{1}.pkl".format(history_seq_len, future_seq_len), "wb") as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    # sliding window size for generating history sequence and target sequence
+    HISTORY_SEQ_LEN = 336
+    FUTURE_SEQ_LEN = 336
+
+    TRAIN_RATIO = 0.6
+    VALID_RATIO = 0.2
+    TARGET_CHANNEL = [0]                   # target channel(s)
+
+    DATASET_NAME = "ETTh1"      # sampling frequency: every 1 hour
+    TOD = True                  # if add time_of_day feature
+    DOW = True                  # if add day_of_week feature
+    DOM = True                  # if add day_of_month feature
+    DOY = True                  # if add day_of_year feature
+
+    OUTPUT_DIR = "datasets/" + DATASET_NAME
+    DATA_FILE_PATH = "datasets/raw_data/{0}/{0}.csv".format(DATASET_NAME)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_dir", type=str,
+                        default=OUTPUT_DIR, help="Output directory.")
+    parser.add_argument("--data_file_path", type=str,
+                        default=DATA_FILE_PATH, help="Raw traffic readings.")
+    parser.add_argument("--history_seq_len", type=int,
+                        default=HISTORY_SEQ_LEN, help="Sequence Length.")
+    parser.add_argument("--future_seq_len", type=int,
+                        default=FUTURE_SEQ_LEN, help="Sequence Length.")
+    parser.add_argument("--tod", type=bool, default=TOD,
+                        help="Add feature time_of_day.")
+    parser.add_argument("--dow", type=bool, default=DOW,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--dom", type=bool, default=DOM,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--doy", type=bool, default=DOY,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--target_channel", type=list,
+                        default=TARGET_CHANNEL, help="Selected channels.")
+    parser.add_argument("--train_ratio", type=float,
+                        default=TRAIN_RATIO, help="Train ratio")
+    parser.add_argument("--valid_ratio", type=float,
+                        default=VALID_RATIO, help="Validate ratio.")
+    args_metr = parser.parse_args()
+
+    # print args
+    print("-"*(20+45+5))
+    for key, value in sorted(vars(args_metr).items()):
+        print("|{0:>20} = {1:<45}|".format(key, str(value)))
+    print("-"*(20+45+5))
+
+    if os.path.exists(args_metr.output_dir):
+        reply = str(input(
+            f"{args_metr.output_dir} exists. Do you want to overwrite it? (y/n)")).lower().strip()
+        if reply[0] != "y":
+            sys.exit(0)
+    else:
+        os.makedirs(args_metr.output_dir)
+    generate_data(args_metr)
---
+++ scripts/data_preparation/ETTh2/generate_training_data.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import argparse
+
+# TODO: remove it when basicts can be installed by pip
+sys.path.append(os.path.abspath(__file__ + "/../../../.."))
+from scripts.data_preparation.ETTh1.generate_training_data import generate_data
+
+
+if __name__ == "__main__":
+    # sliding window size for generating history sequence and target sequence
+    HISTORY_SEQ_LEN = 336
+    FUTURE_SEQ_LEN = 336
+
+    TRAIN_RATIO = 0.6
+    VALID_RATIO = 0.2
+    TARGET_CHANNEL = [0]                   # target channel(s)
+
+    DATASET_NAME = "ETTh2"      # sampling frequency: every 1 hour
+    TOD = True                  # if add time_of_day feature
+    DOW = True                  # if add day_of_week feature
+    DOM = True                  # if add day_of_month feature
+    DOY = True                  # if add day_of_year feature
+
+    OUTPUT_DIR = "datasets/" + DATASET_NAME
+    DATA_FILE_PATH = "datasets/raw_data/{0}/{0}.csv".format(DATASET_NAME)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_dir", type=str,
+                        default=OUTPUT_DIR, help="Output directory.")
+    parser.add_argument("--data_file_path", type=str,
+                        default=DATA_FILE_PATH, help="Raw traffic readings.")
+    parser.add_argument("--history_seq_len", type=int,
+                        default=HISTORY_SEQ_LEN, help="Sequence Length.")
+    parser.add_argument("--future_seq_len", type=int,
+                        default=FUTURE_SEQ_LEN, help="Sequence Length.")
+    parser.add_argument("--tod", type=bool, default=TOD,
+                        help="Add feature time_of_day.")
+    parser.add_argument("--dow", type=bool, default=DOW,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--dom", type=bool, default=DOM,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--doy", type=bool, default=DOY,
+                        help="Add feature day_of_week.")
+    parser.add_argument("--target_channel", type=list,
+                        default=TARGET_CHANNEL, help="Selected channels.")
+    parser.add_argument("--train_ratio", type=float,
+                        default=TRAIN_RATIO, help="Train ratio")
+    parser.add_argument("--valid_ratio", type=float,
+                        default=VALID_RATIO, help="Validate ratio.")
+    args_metr = parser.parse_args()
+
+    # print args
+    print("-"*(20+45+5))
+    for key, value in sorted(vars(args_metr).items()):
+        print("|{0:>20} = {1:<45}|".format(key, str(value)))
+    print("-"*(20+45+5))
+
+    if os.path.exists(args_metr.output_dir):
+        reply = str(input(
+            f"{args_metr.output_dir} exists. Do you want to overwrite it? (y/n)")).lower().strip()
+        if reply[0] != "y":
+            sys.exit(0)
+    else:
+        os.makedirs(args_metr.output_dir)
+    generate_data(args_metr)
