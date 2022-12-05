d90873f92c00532e7f9818f543d586f34bcbff6b
Azusa <61765965+Azusa-Yuan@users.noreply.github.com>
2022-12-05 21:32:56 +0800

    fit higher easy-torch version (#38)
    * fit higher easy-torch version

--- basicts/launcher.py
+++ basicts/launcher.py
@@ -16,4 +16,8 @@ def launch_training(cfg: Union[Dict, str], gpus: str = None, node_rank: int = 0)
     # config checking
     pass
     # launch training based on easytorch
-    easytorch.launch_training(cfg=cfg, gpus=gpus, node_rank=node_rank)
+    try:
+        easytorch.launch_training(cfg=cfg, gpus=gpus, node_rank=node_rank)
+    except:
+        # fit higher easy-torch version
+        easytorch.launch_training(cfg=cfg, devices=gpus, node_rank=node_rank)
