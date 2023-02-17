9f128cc11f375ebac397aae15f90b76fcc056e0c
Zezhi Shao <864453277@qq.com>
2023-02-17 10:19:50 +0800

    fix: 🐛 a bug on Jupyter Notebook

--- basicts/__init__.py
+++ basicts/__init__.py
@@ -1,5 +1,5 @@
 from .launcher import launch_training
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 __all__ = ["__version__", "launch_training"]
--- basicts/data/__init__.py
+++ basicts/data/__init__.py
@@ -1,5 +1,4 @@
 import os
-import platform
 
 from easytorch.utils.registry import scan_modules
 
@@ -8,11 +7,6 @@ from .dataset import TimeSeriesForecastingDataset
 
 __all__ = ["SCALER_REGISTRY", "TimeSeriesForecastingDataset"]
 
-project_dir = os.getcwd()
-if platform.system().lower() == 'windows':
-    # On Windows systems, os.getcwd() will get an uppercase drive letter, such as C:\\Users\\...
-    # However, the drive letter obtained by __file__ is lowercase, such as c:\\Users\\...
-    # TODO: remove these code when this issue is officially fixed in the next EasyTorch version.
-    project_dir = project_dir[0].lower() + project_dir[1:]
-
+# fix bugs on Windows systems and on jupyter
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 scan_modules(project_dir, __file__, ["__init__.py", "registry.py"])
