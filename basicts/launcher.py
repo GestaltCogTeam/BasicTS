7b73af3b68a105bf42a7b9f871f8cceb1f9ae4dc
Zezhi Shao <864453277@qq.com>
2023-04-24 14:13:01 +0800

    pylint

--- basicts/launcher.py
+++ basicts/launcher.py
@@ -4,7 +4,7 @@ from typing import Callable, Dict, Union, Tuple
 import easytorch
 
 
-def launch_runner(cfg: Union[Dict, str], fn: Callable, args: Tuple = (), device_type: str = 'gpu', devices: str = None):
+def launch_runner(cfg: Union[Dict, str], fn: Callable, args: Tuple = (), device_type: str = "gpu", devices: str = None):
     easytorch_version = easytorch.__version__
     if version.parse(easytorch_version) >= version.parse("1.3"):
         easytorch.launch_runner(cfg=cfg, fn=fn, args=args, device_type=device_type, devices=devices)
