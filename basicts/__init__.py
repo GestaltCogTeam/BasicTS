da00c288610ff3e31bda91dfe458788af71ccd56
Zezhi Shao <864453277@qq.com>
2023-09-05 01:36:13 +0800

    update version

--- basicts/__init__.py
+++ basicts/__init__.py
@@ -1,6 +1,6 @@
 from .launcher import launch_training, launch_runner
 from .runners import BaseRunner
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 __all__ = ["__version__", "launch_training", "launch_runner", "BaseRunner"]
