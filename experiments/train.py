a9a2739c75e2d462f98299f804caab35489dced1
Zezhi Shao <864453277@qq.com>
2023-10-31 13:02:34 +0800

    update training script

--- experiments/train.py
+++ experiments/train.py
@@ -15,9 +15,8 @@ torch.set_num_threads(3) # aviod high cpu avg usage
 
 def parse_args():
     parser = ArgumentParser(description="Run time series forecasting model in BasicTS framework!")
-    parser.add_argument("-c", "--cfg", default="baselines/DGCRN/DGCRN_METR-LA.py", help="training config")
-    # parser.add_argument("-c", "--cfg", default="baselines/STGODE/STGODE_PEMS08.py", help="training config")
-    parser.add_argument("-g", "--gpus", default="3", help="visible gpus")
+    parser.add_argument("-c", "--cfg", default="baselines/STID/METR-LA.py", help="training config")
+    parser.add_argument("-g", "--gpus", default="0", help="visible gpus")
     return parser.parse_args()
 
 if __name__ == "__main__":
