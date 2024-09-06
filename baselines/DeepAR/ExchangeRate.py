0d8c6b20e069ccd075dc31a92f94c49c34c61d1d
Zezhi Shao <864453277@qq.com>
2024-09-06 13:16:42 +0800

    update DeepAR configs.

--- baselines/DeepAR/ExchangeRate.py
+++ baselines/DeepAR/ExchangeRate.py
@@ -16,7 +16,8 @@ from .loss import gaussian_loss
 # Dataset & Metrics configuration
 DATA_NAME = 'ExchangeRate'  # Dataset name
 regular_settings = get_regular_settings(DATA_NAME)
-INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
+# INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
+INPUT_LEN = 96 # faster training
 OUTPUT_LEN = regular_settings['OUTPUT_LEN']  # Length of output sequence
 TRAIN_VAL_TEST_RATIO = regular_settings['TRAIN_VAL_TEST_RATIO']  # Train/Validation/Test split ratios
 NORM_EACH_CHANNEL = regular_settings['NORM_EACH_CHANNEL'] # Whether to normalize each channel of the data
