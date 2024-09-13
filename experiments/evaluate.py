4035980ebc19d4ec8e72fa7b22cd16b9a8329130
Zezhi Shao <864453277@qq.com>
2024-09-13 11:27:30 +0800

    feat: 🎸 update data and prediction visualization scripts

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -8,11 +8,11 @@ import basicts
 
 def parse_args():
     parser = ArgumentParser(description="Evaluate time series forecasting model in BasicTS framework!")
-    parser.add_argument("-cfg", "--config", default="examples/complete_config.py", help="training config")
-    parser.add_argument("-ckpt", "--checkpoint", default="")
-    parser.add_argument("-g", "--gpus", default="0")
+    parser.add_argument("-cfg", "--config", default="baselines/STID/PEMS08_LTSF.py", help="training config") # enter your config file path
+    parser.add_argument("-ckpt", "--checkpoint", default="checkpoints/STID/PEMS08_100_336_336/97d131cadc14bd2b9ffa892d59d55129/STID_best_val_MAE.pt") # enter your own checkpoint file path
+    parser.add_argument("-g", "--gpus", default="5")
     parser.add_argument("-d", "--device_type", default="gpu")
-    parser.add_argument("-b", "--batch_size", default=None)
+    parser.add_argument("-b", "--batch_size", default=None) # use the batch size in the config file
     
     return parser.parse_args()
 
