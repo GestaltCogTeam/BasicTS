7a18c5729985737f35e26e2c0360f08fd0bcc515
Yisong Fu <jokerfulyfock@gmail.com>
2024-10-23 23:41:09 +0800

    feat: 🎸 improve code readability
    moving runner.preprocess and runner.postprocessing functions into
    runner.forward.

--- baselines/STEP/runner/tsformer_runner.py
+++ baselines/STEP/runner/tsformer_runner.py
@@ -3,43 +3,12 @@ from typing import Optional, Dict
 import torch
 from tqdm import tqdm
 from easytorch.utils.dist import master_only
-from basicts.runners import BaseTimeSeriesForecastingRunner
+from basicts.runners import SimpleTimeSeriesForecastingRunner
 
 
-class TSFormerRunner(BaseTimeSeriesForecastingRunner):
+class TSFormerRunner(SimpleTimeSeriesForecastingRunner):
     def __init__(self, cfg: dict):
         super().__init__(cfg)
-        self.forward_features = cfg["MODEL"].get("FORWARD_FEATURES", None)
-        self.target_features = cfg["MODEL"].get("TARGET_FEATURES", None)
-
-    def select_input_features(self, data: torch.Tensor) -> torch.Tensor:
-        """Select input features and reshape data to fit the target model.
-
-        Args:
-            data (torch.Tensor): input history data, shape [B, L, N, C].
-
-        Returns:
-            torch.Tensor: reshaped data
-        """
-
-        # select feature using self.forward_features
-        if self.forward_features is not None:
-            data = data[:, :, :, self.forward_features]
-        return data
-
-    def select_target_features(self, data: torch.Tensor) -> torch.Tensor:
-        """Select target features and reshape data back to the BasicTS framework
-
-        Args:
-            data (torch.Tensor): prediction of the model with arbitrary shape.
-
-        Returns:
-            torch.Tensor: reshaped data with shape [B, L, N, C]
-        """
-
-        # select feature using self.target_features
-        data = data[:, :, :, self.target_features]
-        return data
 
     def forward(self, data: tuple, epoch:int = None, iter_num: int = None, train:bool = True, **kwargs) -> tuple:
         """feed forward process for train, val, and test. Note that the outputs are NOT re-scaled.
@@ -54,6 +23,7 @@ class TSFormerRunner(BaseTimeSeriesForecastingRunner):
             tuple: (prediction, real_value)
         """
 
+        data = self.preprocessing(data)
         # Preprocess input data
         future_data, history_data = data['target'], data['inputs']
         history_data = self.to_running_device(history_data)  # Shape: [B, L, N, C]
@@ -66,7 +36,8 @@ class TSFormerRunner(BaseTimeSeriesForecastingRunner):
         # feed forward
         reconstruction_masked_tokens, label_masked_tokens = self.model(history_data=history_data, future_data=None, batch_seen=iter_num, epoch=epoch)
         results = {'prediction': reconstruction_masked_tokens, 'target': label_masked_tokens, 'inputs': history_data}
-        return results
+        model_return = self.postprocessing(model_return)
+        return results 
 
     @torch.no_grad()
     @master_only
