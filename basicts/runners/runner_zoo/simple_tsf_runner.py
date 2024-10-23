6f832b215818b2e40d21c4c99e018ca50422a387
Zezhi Shao <864453277@qq.com>
2024-10-23 22:31:16 +0800

    Move the data pre- and post-processing methods to simple_tsf_runner.

--- basicts/runners/runner_zoo/simple_tsf_runner.py
+++ basicts/runners/runner_zoo/simple_tsf_runner.py
@@ -18,6 +18,102 @@ class SimpleTimeSeriesForecastingRunner(BaseTimeSeriesForecastingRunner):
         self.forward_features = cfg['MODEL'].get('FORWARD_FEATURES', None)
         self.target_features = cfg['MODEL'].get('TARGET_FEATURES', None)
 
+    def preprocessing(self, input_data: Dict) -> Dict:
+        """Preprocess data.
+
+        Args:
+            input_data (Dict): Dictionary containing data to be processed.
+
+        Returns:
+            Dict: Processed data.
+        """
+
+        if self.scaler is not None:
+            input_data['target'] = self.scaler.transform(input_data['target'])
+            input_data['inputs'] = self.scaler.transform(input_data['inputs'])
+        # TODO: add more preprocessing steps as needed.
+        return input_data
+
+    def postprocessing(self, input_data: Dict) -> Dict:
+        """Postprocess data.
+
+        Args:
+            input_data (Dict): Dictionary containing data to be processed.
+
+        Returns:
+            Dict: Processed data.
+        """
+
+        # rescale data
+        if self.scaler is not None and self.scaler.rescale:
+            input_data['prediction'] = self.scaler.inverse_transform(input_data['prediction'])
+            input_data['target'] = self.scaler.inverse_transform(input_data['target'])
+            input_data['inputs'] = self.scaler.inverse_transform(input_data['inputs'])
+
+        # subset forecasting
+        if self.target_time_series is not None:
+            input_data['target'] = input_data['target'][:, :, self.target_time_series, :]
+            input_data['prediction'] = input_data['prediction'][:, :, self.target_time_series, :]
+
+        # TODO: add more postprocessing steps as needed.
+        return input_data
+
+    def forward(self, data: Dict, epoch: int = None, iter_num: int = None, train: bool = True, **kwargs) -> Dict:
+        """
+        Performs the forward pass for training, validation, and testing. 
+
+        Args:
+            data (Dict): A dictionary containing 'target' (future data) and 'inputs' (history data) (normalized by self.scaler).
+            epoch (int, optional): Current epoch number. Defaults to None.
+            iter_num (int, optional): Current iteration number. Defaults to None.
+            train (bool, optional): Indicates whether the forward pass is for training. Defaults to True.
+
+        Returns:
+            Dict: A dictionary containing the keys:
+                  - 'inputs': Selected input features.
+                  - 'prediction': Model predictions.
+                  - 'target': Selected target features.
+
+        Raises:
+            AssertionError: If the shape of the model output does not match [B, L, N].
+        """
+
+        data = self.preprocessing(data)
+
+        # Preprocess input data
+        future_data, history_data = data['target'], data['inputs']
+        history_data = self.to_running_device(history_data)  # Shape: [B, L, N, C]
+        future_data = self.to_running_device(future_data)    # Shape: [B, L, N, C]
+        batch_size, length, num_nodes, _ = future_data.shape
+
+        # Select input features
+        history_data = self.select_input_features(history_data)
+        future_data_4_dec = self.select_input_features(future_data)
+        
+        if not train:
+            # For non-training phases, use only temporal features
+            future_data_4_dec[..., 0] = torch.empty_like(future_data_4_dec[..., 0])
+
+        # Forward pass through the model
+        model_return = self.model(history_data=history_data, future_data=future_data_4_dec, 
+                                  batch_seen=iter_num, epoch=epoch, train=train)
+
+        # Parse model return
+        if isinstance(model_return, torch.Tensor):
+            model_return = {'prediction': model_return}
+        if 'inputs' not in model_return:
+            model_return['inputs'] = self.select_target_features(history_data)
+        if 'target' not in model_return:
+            model_return['target'] = self.select_target_features(future_data)
+
+        # Ensure the output shape is correct
+        assert list(model_return['prediction'].shape)[:3] == [batch_size, length, num_nodes], \
+            "The shape of the output is incorrect. Ensure it matches [B, L, N, C]."
+
+        model_return = self.postprocessing(model_return)
+
+        return model_return
+
     def select_input_features(self, data: torch.Tensor) -> torch.Tensor:
         """
         Selects input features based on the forward features specified in the configuration.
@@ -60,55 +156,3 @@ class SimpleTimeSeriesForecastingRunner(BaseTimeSeriesForecastingRunner):
 
         data = data[:, :, self.target_time_series, :]
         return data
-
-    def forward(self, data: Dict, epoch: int = None, iter_num: int = None, train: bool = True, **kwargs) -> Dict:
-        """
-        Performs the forward pass for training, validation, and testing. 
-
-        Args:
-            data (Dict): A dictionary containing 'target' (future data) and 'inputs' (history data) (normalized by self.scaler).
-            epoch (int, optional): Current epoch number. Defaults to None.
-            iter_num (int, optional): Current iteration number. Defaults to None.
-            train (bool, optional): Indicates whether the forward pass is for training. Defaults to True.
-
-        Returns:
-            Dict: A dictionary containing the keys:
-                  - 'inputs': Selected input features.
-                  - 'prediction': Model predictions.
-                  - 'target': Selected target features.
-
-        Raises:
-            AssertionError: If the shape of the model output does not match [B, L, N].
-        """
-
-        # Preprocess input data
-        future_data, history_data = data['target'], data['inputs']
-        history_data = self.to_running_device(history_data)  # Shape: [B, L, N, C]
-        future_data = self.to_running_device(future_data)    # Shape: [B, L, N, C]
-        batch_size, length, num_nodes, _ = future_data.shape
-
-        # Select input features
-        history_data = self.select_input_features(history_data)
-        future_data_4_dec = self.select_input_features(future_data)
-        
-        if not train:
-            # For non-training phases, use only temporal features
-            future_data_4_dec[..., 0] = torch.empty_like(future_data_4_dec[..., 0])
-
-        # Forward pass through the model
-        model_return = self.model(history_data=history_data, future_data=future_data_4_dec, 
-                                  batch_seen=iter_num, epoch=epoch, train=train)
-
-        # Parse model return
-        if isinstance(model_return, torch.Tensor):
-            model_return = {'prediction': model_return}
-        if 'inputs' not in model_return:
-            model_return['inputs'] = self.select_target_features(history_data)
-        if 'target' not in model_return:
-            model_return['target'] = self.select_target_features(future_data)
-
-        # Ensure the output shape is correct
-        assert list(model_return['prediction'].shape)[:3] == [batch_size, length, num_nodes], \
-            "The shape of the output is incorrect. Ensure it matches [B, L, N, C]."
-
-        return model_return
