90a9eb3a9e3718c66d0c804bae6b7e9beada1b23
superarthurlx <40826115+superarthurlx@users.noreply.github.com>
2024-10-24 15:21:26 +0800

    add baseline fredformer (#163)
    * 'add-fredformer'
    
    ---------
    
    Co-authored-by: Yisong Fu <jokerfulyfock@gmail.com>

---
+++ baselines/Fredformer/arch/cross_Transformer_nys.py
@@ -0,0 +1,184 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+from einops import rearrange
+from math import ceil
+
+from einops import rearrange, reduce
+
+# NystromTrans
+
+def exists(val):
+    return val is not None
+
+# NystromParam
+def moore_penrose_iter_pinv(x, iters=6):
+    device = x.device
+
+    abs_x = torch.abs(x)
+    col = abs_x.sum(dim=-1)
+    row = abs_x.sum(dim=-2)
+    z = rearrange(x, '... i j -> ... j i') / (torch.max(col) * torch.max(row))
+
+    I = torch.eye(x.shape[-1], device=device)
+    I = rearrange(I, 'i j -> () i j')
+
+    for _ in range(iters):
+        xz = x @ z
+        z = 0.25 * z @ (13 * I - (xz @ (15 * I - (xz @ (7 * I - xz)))))
+
+    return z
+
+class PreNorm(nn.Module): 
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.5):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads, dim_head, num_landmarks,
+                 pinv_iterations, eps, dropout=0.5):
+        super().__init__()
+        inner_dim = dim_head * heads  ##32(4*8)
+        project_out = not (heads == 1 and dim_head == dim)
+
+        ## NystromParam
+        self.eps = eps
+        self.num_landmarks = num_landmarks
+        self.pinv_iterations = pinv_iterations
+        ## NystromParam
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim=-1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)  ##better to dim to dim*3
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x, mask=None, return_attn=False):
+        b, n, _, h, m, iters, eps = *x.shape, self.heads, self.num_landmarks, self.pinv_iterations, self.eps
+
+        remainder = n % m
+        if remainder > 0:
+            padding = m - (n % m)
+            x = F.pad(x, (0, 0, padding, 0), value=0)
+
+            if exists(mask):
+                mask = F.pad(mask, (padding, 0), value=False)
+
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
+
+        q = q * self.scale
+
+        # generate landmarks by sum reduction, and then calculate mean using the mask
+
+        l = ceil(n / m)
+        landmark_einops_eq = '... (n l) d -> ... n d'
+        q_landmarks = reduce(q, landmark_einops_eq, 'sum', l=l)
+        k_landmarks = reduce(k, landmark_einops_eq, 'sum', l=l)
+
+        # calculate landmark mask, and also get sum of non-masked elements in preparation for masked mean
+
+        divisor = l
+        if exists(mask):
+            mask_landmarks_sum = reduce(mask, '... (n l) -> ... n', 'sum', l=l)
+            divisor = mask_landmarks_sum[..., None] + eps
+            mask_landmarks = mask_landmarks_sum > 0
+
+        # masked mean (if mask exists)
+
+        q_landmarks /= divisor
+        k_landmarks /= divisor
+
+        # similarities
+
+        einops_eq = '... i d, ... j d -> ... i j'
+        sim1 = einsum(einops_eq, q, k_landmarks)
+        sim2 = einsum(einops_eq, q_landmarks, k_landmarks)
+        sim3 = einsum(einops_eq, q_landmarks, k)
+
+        # eq (15) in the paper and aggregate values
+
+        attn1, attn2, attn3 = map(lambda t: t.softmax(dim=-1), (sim1, sim2, sim3))
+        attn2_inv = moore_penrose_iter_pinv(attn2, iters)
+
+        out = (attn1 @ attn2_inv) @ (attn3 @ v)
+
+        # add depth-wise conv residual of values
+
+        #         if self.residual:
+        #             out += self.res_conv(v)
+
+        # merge and combine heads
+
+        out = rearrange(out, 'b h n d -> b n (h d)', h=h)
+        out = self.to_out(out)
+        out = out[:, -n:]
+
+        if return_attn:
+            attn = attn1 @ attn2_inv @ attn3
+            return out, attn
+
+        return out
+
+
+
+class Trans_C(nn.Module):
+    def __init__(self, *, dim, depth, heads, mlp_dim, dim_head, dropout , patch_dim, horizon, d_model):
+        super().__init__()
+        
+        self.dim = dim
+        self.patch_dim = patch_dim
+        self.to_patch_embedding = nn.Sequential(nn.Linear(patch_dim, dim),nn.Dropout(dropout))
+        self.dropout = nn.Dropout(dropout)
+        self.transformer = Nystromformer(dim, depth, heads, dim_head, mlp_dim, num_landmarks=5,pinv_iterations = 6,eps=1e-8,dropout=dropout)
+        
+        self.mlp_head = nn.Linear(dim, d_model)
+
+    def forward(self, x):
+        
+        x = self.to_patch_embedding(x)
+        #x = self.dropout(x)
+        x = self.transformer(x)
+        x = self.mlp_head(x).squeeze()
+        return x
+
+
+
+class Nystromformer(nn.Module):  ##Register the blocks into whole network
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, num_landmarks,
+                 pinv_iterations, eps, dropout=0.5):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, num_landmarks=num_landmarks,
+                                       pinv_iterations=pinv_iterations, eps=eps, dropout=dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
+            ]))
+
+    def forward(self, x, mask=None):
+        for attn, ff in self.layers:
+            x = attn(x, mask=mask) + x
+            x = ff(x) + x
+        return x
