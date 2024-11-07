3e3376aa1b5f8b2f37cf3e053cd85855fde1e3d9
lx <arthurlx94@163.com>
2024-11-07 17:42:29 +0800

    'first-add-dlinear-glaff'

---
+++ baselines/DLinear_GLAFF/Weather.py
@@ -0,0 +1,148 @@
+import os
+import sys
+from easydict import EasyDict
+sys.path.append(os.path.abspath(__file__ + '/../../..'))
+
+from basicts.metrics import masked_mae, masked_mse, masked_mape, masked_rmse
+from basicts.data import TimeSeriesForecastingDataset
+from basicts.runners import SimpleTimeSeriesForecastingRunner
+from basicts.scaler import ZScoreScaler
+from basicts.utils import get_regular_settings
+
+from .arch import DLinear_GLAFF
+
+############################## Hot Parameters ##############################
+# Dataset & Metrics configuration
+DATA_NAME = 'Weather'  # Dataset name
+regular_settings = get_regular_settings(DATA_NAME)
+INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
+OUTPUT_LEN = regular_settings['OUTPUT_LEN']  # Length of output sequence
+TRAIN_VAL_TEST_RATIO = regular_settings['TRAIN_VAL_TEST_RATIO']  # Train/Validation/Test split ratios
+NORM_EACH_CHANNEL = regular_settings['NORM_EACH_CHANNEL'] # Whether to normalize each channel of the data
+RESCALE = regular_settings['RESCALE'] # Whether to rescale the data
+NULL_VAL = regular_settings['NULL_VAL'] # Null value in the data
+# Model architecture and parameters
+MODEL_ARCH = DLinear_GLAFF
+MODEL_PARAM = {
+    "hist_len": INPUT_LEN,
+    "pred_len": OUTPUT_LEN,
+    "individual": False,
+    "enc_in": 21,
+    "glaff": True,
+    "q": 0.75,                                  # quantile
+    "dim": 512,                                 # dimension of hidden state
+    "dff": 2048,                                # dimension of feed forward
+    "head_num": 8,
+    "dropout": 0.1,
+    "layer_num": 2,
+    "num_time_features": 3,                     # number of used time features
+    "time_of_day_size": 144,
+    "day_of_week_size": 7,
+    "day_of_month_size": 31,
+    "day_of_year_size": 366
+}
+NUM_EPOCHS = 100
+
+############################## General Configuration ##############################
+CFG = EasyDict()
+# General settings
+CFG.DESCRIPTION = 'An Example Config'
+CFG.GPU_NUM = 1 # Number of GPUs to use (0 for CPU mode)
+# Runner
+CFG.RUNNER = SimpleTimeSeriesForecastingRunner
+
+############################## Dataset Configuration ##############################
+CFG.DATASET = EasyDict()
+# Dataset settings
+CFG.DATASET.NAME = DATA_NAME
+CFG.DATASET.TYPE = TimeSeriesForecastingDataset
+CFG.DATASET.PARAM = EasyDict({
+    'dataset_name': DATA_NAME,
+    'train_val_test_ratio': TRAIN_VAL_TEST_RATIO,
+    'input_len': INPUT_LEN,
+    'output_len': OUTPUT_LEN,
+    # 'mode' is automatically set by the runner
+})
+
+############################## Scaler Configuration ##############################
+CFG.SCALER = EasyDict()
+# Scaler settings
+CFG.SCALER.TYPE = ZScoreScaler # Scaler class
+CFG.SCALER.PARAM = EasyDict({
+    'dataset_name': DATA_NAME,
+    'train_ratio': TRAIN_VAL_TEST_RATIO[0],
+    'norm_each_channel': NORM_EACH_CHANNEL,
+    'rescale': RESCALE,
+})
+
+############################## Model Configuration ##############################
+CFG.MODEL = EasyDict()
+# Model settings
+CFG.MODEL.NAME = MODEL_ARCH.__name__
+CFG.MODEL.ARCH = MODEL_ARCH
+CFG.MODEL.PARAM = MODEL_PARAM
+CFG.MODEL.FORWARD_FEATURES = [0, 1, 2, 3]
+CFG.MODEL.TARGET_FEATURES = [0]
+
+############################## Metrics Configuration ##############################
+
+CFG.METRICS = EasyDict()
+# Metrics settings
+CFG.METRICS.FUNCS = EasyDict({
+                                'MAE': masked_mae,
+                                'MSE': masked_mse,
+                                'RMSE': masked_rmse,
+                                'MAPE': masked_mape
+                            })
+CFG.METRICS.TARGET = 'MSE'
+CFG.METRICS.NULL_VAL = NULL_VAL
+
+############################## Training Configuration ##############################
+CFG.TRAIN = EasyDict()
+CFG.TRAIN.NUM_EPOCHS = NUM_EPOCHS
+CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+    'checkpoints',
+    MODEL_ARCH.__name__,
+    '_'.join([DATA_NAME, str(CFG.TRAIN.NUM_EPOCHS), str(INPUT_LEN), str(OUTPUT_LEN)])
+)
+CFG.TRAIN.LOSS = masked_mae
+# Optimizer settings
+CFG.TRAIN.OPTIM = EasyDict()
+CFG.TRAIN.OPTIM.TYPE = "Adam"
+CFG.TRAIN.OPTIM.PARAM = {
+    "lr": 0.0001
+}
+# Learning rate scheduler settings
+CFG.TRAIN.LR_SCHEDULER = EasyDict()
+CFG.TRAIN.LR_SCHEDULER.TYPE = "MultiStepLR"
+CFG.TRAIN.LR_SCHEDULER.PARAM = {
+    "milestones": [1, 25],
+    "gamma": 0.5
+}
+CFG.TRAIN.CLIP_GRAD_PARAM = {
+    'max_norm': 5.0
+}
+# Train data loader settings
+CFG.TRAIN.DATA = EasyDict()
+CFG.TRAIN.DATA.BATCH_SIZE = 64
+CFG.TRAIN.DATA.SHUFFLE = True
+
+############################## Validation Configuration ##############################
+CFG.VAL = EasyDict()
+CFG.VAL.INTERVAL = 1
+CFG.VAL.DATA = EasyDict()
+CFG.VAL.DATA.BATCH_SIZE = 64
+
+############################## Test Configuration ##############################
+CFG.TEST = EasyDict()
+CFG.TEST.INTERVAL = 1
+CFG.TEST.DATA = EasyDict()
+CFG.TEST.DATA.BATCH_SIZE = 64
+
+############################## Evaluation Configuration ##############################
+
+CFG.EVAL = EasyDict()
+
+# Evaluation parameters
+CFG.EVAL.HORIZONS = [12, 24, 48, 96]
+CFG.EVAL.USE_GPU = True # Whether to use GPU for evaluation. Default: True
---
+++ baselines/DLinear_GLAFF/arch/glaff.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+from argparse import Namespace
+
+class Plugin(nn.Module):
+    def __init__(self, **model_args):
+        super(Plugin, self).__init__()
+        args = Namespace(**model_args)
+        self.args = args
+        self.channel = args.enc_in
+
+        self.q = args.q
+        self.hist_len = args.hist_len
+        self.pred_len = args.pred_len
+
+        self.Encoder = nn.Sequential(
+            nn.Linear(6, args.dim),
+            nn.TransformerEncoder(
+                nn.TransformerEncoderLayer(
+                    d_model=args.dim,
+                    nhead=args.head_num,
+                    dim_feedforward=args.dff,
+                    dropout=args.dropout,
+                    activation='gelu',
+                    batch_first=True,
+                ),
+                num_layers=args.layer_num,
+                norm=nn.LayerNorm(args.dim)
+            ),
+            nn.Linear(args.dim, self.channel)
+        )
+
+        self.MLP = nn.Sequential(
+            nn.Linear(self.hist_len, args.dff),
+            nn.GELU(),
+            nn.Linear(args.dff, 2),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, x_enc_true, x_mark_enc, x_dec_pred, x_mark_dec):
+        # map
+        x_enc_map = self.Encoder(x_mark_enc)
+        x_dec_map = self.Encoder(x_mark_dec)
+
+        # denormalize
+        scale_true = torch.quantile(x_enc_true, self.q, 1, True) - torch.quantile(x_enc_true, 1 - self.q, 1, True)
+        scale_map = torch.quantile(x_enc_map, self.q, 1, True) - torch.quantile(x_enc_map, 1 - self.q, 1, True)
+        stdev = scale_true / scale_map
+        means = torch.median(x_enc_true - x_enc_map * stdev, dim=1, keepdim=True)[0]
+        x_enc_map = x_enc_map * stdev + means
+        x_dec_map = x_dec_map * stdev + means
+
+        # combine
+        error = x_enc_true - x_enc_map
+        weight = self.MLP(error.permute(0, 2, 1)).unsqueeze(1)
+        x_dec = torch.stack([x_dec_map, x_dec_pred], dim=-1)
+        pred = torch.sum(x_dec * weight, dim=-1)
+
+        return pred, x_enc_map, x_dec_map
