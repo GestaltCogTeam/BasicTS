c257af78ca18976d8dc70c280bed6637a65faa80
lx <arthurlx94@163.com>
2024-11-19 17:16:52 +0800

    'add-SOFTS'

---
+++ baselines/SOFTS/arch/softs_arch.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from argparse import Namespace
+from basicts.utils import data_transformation_4_xformer
+
+from .Embed import DataEmbedding_inverted
+from .Transformer_EncDec import Encoder, EncoderLayer
+
+
+class STAR(nn.Module):
+    def __init__(self, d_series, d_core):
+        super(STAR, self).__init__()
+        """
+        STar Aggregate-Redistribute Module
+        """
+
+        self.gen1 = nn.Linear(d_series, d_series)
+        self.gen2 = nn.Linear(d_series, d_core)
+        self.gen3 = nn.Linear(d_series + d_core, d_series)
+        self.gen4 = nn.Linear(d_series, d_series)
+
+    def forward(self, input, *args, **kwargs):
+        batch_size, channels, d_series = input.shape
+
+        # set FFN
+        combined_mean = F.gelu(self.gen1(input))
+        combined_mean = self.gen2(combined_mean)
+
+        # stochastic pooling
+        if self.training:
+            ratio = F.softmax(combined_mean, dim=1)
+            ratio = ratio.permute(0, 2, 1)
+            ratio = ratio.reshape(-1, channels)
+            indices = torch.multinomial(ratio, 1)
+            indices = indices.view(batch_size, -1, 1).permute(0, 2, 1)
+            combined_mean = torch.gather(combined_mean, 1, indices)
+            combined_mean = combined_mean.repeat(1, channels, 1)
+        else:
+            weight = F.softmax(combined_mean, dim=1)
+            combined_mean = torch.sum(combined_mean * weight, dim=1, keepdim=True).repeat(1, channels, 1)
+
+        # mlp fusion
+        combined_mean_cat = torch.cat([input, combined_mean], -1)
+        combined_mean_cat = F.gelu(self.gen3(combined_mean_cat))
+        combined_mean_cat = self.gen4(combined_mean_cat)
+        output = combined_mean_cat
+
+        return output, None
+
+
+class SOFTS(nn.Module):
+    '''
+    Paper: SOFTS: Efficient Multivariate Time Series Forecasting with Series-Core Fusion
+    Official Code: https://github.com/Secilia-Cxy/SOFTS
+    Link: https://xiucheng.org/assets/pdfs/nips24-sumba.pdf
+    Venue: NeurIPS 2024
+    Task: Long-term Time Series Forecasting
+    '''
+    def __init__(self, **model_args):
+        super(SOFTS, self).__init__()
+        configs = Namespace(**model_args)
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        # Embedding
+        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.dropout)
+        self.use_norm = configs.use_norm
+        self.time_of_day_size = configs.time_of_day_size
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    STAR(configs.d_model, configs.d_core),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation,
+                ) for l in range(configs.e_layers)
+            ],
+        )
+
+        # Decoder
+        self.projection = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+
+    def forecast(self, x_enc, x_mark_enc):
+        # Normalization from Non-stationary Transformer
+        if self.use_norm:
+            means = x_enc.mean(1, keepdim=True).detach()
+            x_enc = x_enc - means
+            stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+            x_enc /= stdev
+
+        _, _, N = x_enc.shape
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+        dec_out = self.projection(enc_out).permute(0, 2, 1)[:, :, :N]
+
+        # De-Normalization from Non-stationary Transformer
+        if self.use_norm:
+            dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+            dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        return dec_out
+
+    def forward_xformer(self, x_enc, x_mark_enc):
+        dec_out = self.forecast(x_enc, x_mark_enc)
+        return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+
+
+    def forward(self, history_data: torch.Tensor, future_data: torch.Tensor, batch_seen: int, epoch: int, train: bool,
+                **kwargs) -> torch.Tensor:
+        """
+
+        Args:
+            history_data (Tensor): Input data with shape: [B, L1, N, C]
+            future_data (Tensor): Future data with shape: [B, L2, N, C]
+
+        Returns:
+            torch.Tensor: outputs with shape [B, L2, N, 1]
+        """
+
+        # change MinuteOfDay to MinuteOfHour
+        history_data[..., 1] = history_data[..., 1] * self.time_of_day_size // (self.time_of_day_size / 24) / 23.0
+        x_enc, x_mark_enc, x_dec, x_mark_dec = data_transformation_4_xformer(history_data=history_data,
+                                                                             future_data=future_data,
+                                                                             start_token_len=0)
+        #print(x_mark_enc.shape, x_mark_dec.shape)
+        prediction = self.forward_xformer(x_enc=x_enc, x_mark_enc=x_mark_enc)
+        return prediction.unsqueeze(-1)
+
+    
+
+
+
+
+
