80361c61b655da3bf00a01cc9716229ebb74a676
Zezhi Shao <864453277@qq.com>
2024-11-27 17:19:11 +0800

    docs: ✏️ update configs of iTransformer

--- baselines/iTransformer/Traffic.py
+++ baselines/iTransformer/Traffic.py
@@ -2,7 +2,7 @@ import os
 import sys
 from easydict import EasyDict
 sys.path.append(os.path.abspath(__file__ + '/../../..'))
-from basicts.metrics import masked_mae, masked_mse
+from basicts.metrics import masked_mae, masked_mse, masked_mape, masked_rmse
 from basicts.data import TimeSeriesForecastingDataset
 from basicts.runners import SimpleTimeSeriesForecastingRunner
 from basicts.scaler import ZScoreScaler
@@ -12,17 +12,17 @@ from .arch import iTransformer
 
 ############################## Hot Parameters ##############################
 # Dataset & Metrics configuration
-DATA_NAME = 'Electricity'  # Dataset name
+DATA_NAME = 'Traffic'  # Dataset name
 regular_settings = get_regular_settings(DATA_NAME)
-INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
-OUTPUT_LEN = regular_settings['OUTPUT_LEN']  # Length of output sequence
+INPUT_LEN = 96
+OUTPUT_LEN = 720
 TRAIN_VAL_TEST_RATIO = regular_settings['TRAIN_VAL_TEST_RATIO']  # Train/Validation/Test split ratios
 NORM_EACH_CHANNEL = regular_settings['NORM_EACH_CHANNEL'] # Whether to normalize each channel of the data
 RESCALE = regular_settings['RESCALE'] # Whether to rescale the data
 NULL_VAL = regular_settings['NULL_VAL'] # Null value in the data
 # Model architecture and parameters
 MODEL_ARCH = iTransformer
-NUM_NODES = 321
+NUM_NODES = 862
 MODEL_PARAM = {
     "enc_in": NUM_NODES,                        # num nodes
     "dec_in": NUM_NODES,
@@ -36,7 +36,7 @@ MODEL_PARAM = {
     "d_model": 512,
     "moving_avg": 25,                           # window size of moving average. This is a CRUCIAL hyper-parameter.
     "n_heads": 8,
-    "e_layers": 3,                              # num of encoder layers
+    "e_layers": 4,                              # num of encoder layers
     "d_layers": 1,                              # num of decoder layers
     "d_ff": 512,
     "distil": True,
@@ -53,7 +53,7 @@ MODEL_PARAM = {
     "day_of_month_size": 31,
     "day_of_year_size": 366
     }
-NUM_EPOCHS = 100
+NUM_EPOCHS = 20
 
 ############################## General Configuration ##############################
 CFG = EasyDict()
@@ -102,9 +102,11 @@ CFG.METRICS = EasyDict()
 # Metrics settings
 CFG.METRICS.FUNCS = EasyDict({
                                 'MAE': masked_mae,
-                                'MSE': masked_mse
+                                'MSE': masked_mse,
+                                'RMSE': masked_rmse,
+                                'MAPE': masked_mape
                             })
-CFG.METRICS.TARGET = 'MAE'
+CFG.METRICS.TARGET = 'MSE'
 CFG.METRICS.NULL_VAL = NULL_VAL
 
 ############################## Training Configuration ##############################
@@ -120,13 +122,13 @@ CFG.TRAIN.LOSS = masked_mae
 CFG.TRAIN.OPTIM = EasyDict()
 CFG.TRAIN.OPTIM.TYPE = "Adam"
 CFG.TRAIN.OPTIM.PARAM = {
-    "lr": 0.0005,
+    "lr": 0.001,
 }
 # Learning rate scheduler settings
 CFG.TRAIN.LR_SCHEDULER = EasyDict()
 CFG.TRAIN.LR_SCHEDULER.TYPE = "MultiStepLR"
 CFG.TRAIN.LR_SCHEDULER.PARAM = {
-    "milestones": [1, 25, 50],
+    "milestones": [5, 10],
     "gamma": 0.5
 }
 CFG.TRAIN.CLIP_GRAD_PARAM = {
@@ -134,24 +136,24 @@ CFG.TRAIN.CLIP_GRAD_PARAM = {
 }
 # Train data loader settings
 CFG.TRAIN.DATA = EasyDict()
-CFG.TRAIN.DATA.BATCH_SIZE = 64
+CFG.TRAIN.DATA.BATCH_SIZE = 32
 CFG.TRAIN.DATA.SHUFFLE = True
 
 ############################## Validation Configuration ##############################
 CFG.VAL = EasyDict()
 CFG.VAL.INTERVAL = 1
 CFG.VAL.DATA = EasyDict()
-CFG.VAL.DATA.BATCH_SIZE = 64
+CFG.VAL.DATA.BATCH_SIZE = 32
 
 ############################## Test Configuration ##############################
 CFG.TEST = EasyDict()
 CFG.TEST.INTERVAL = 1
 CFG.TEST.DATA = EasyDict()
-CFG.TEST.DATA.BATCH_SIZE = 64
+CFG.TEST.DATA.BATCH_SIZE = 32
 
 ############################## Evaluation Configuration ##############################
 
 CFG.EVAL = EasyDict()
 
 # Evaluation parameters
-CFG.EVAL.USE_GPU = True # Whether to use GPU for evaluation. Default: True
+CFG.EVAL.USE_GPU = False # Whether to use GPU for evaluation. Default: True
