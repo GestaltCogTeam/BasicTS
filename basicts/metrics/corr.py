398bbc4bcf2227324582acb3543564953e8d9abf
lx <arthurlx94@163.com>
2025-01-24 16:33:06 +0800

    'update4isort'

--- basicts/metrics/corr.py
+++ basicts/metrics/corr.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 
+
 def masked_corr(prediction: torch.Tensor, target: torch.Tensor, null_val: float = np.nan) -> torch.Tensor:
     """
     Calculate the Masked Pearson Correlation Coefficient between the predicted and target values,
