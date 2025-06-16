f362f96632c4644ab09400186d32ddd7d4d956f2
Zezhi Shao <864453277@qq.com>
2025-06-16 15:27:48 +0800

    update S-Mamba

--- baselines/S_Mamba/Electricity.py
+++ baselines/S_Mamba/Electricity.py
@@ -15,7 +15,8 @@ from .arch import S_Mamba
 # Dataset & Metrics configuration
 DATA_NAME = 'Electricity'  # Dataset name
 regular_settings = get_regular_settings(DATA_NAME)
-INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
+# INPUT_LEN = regular_settings['INPUT_LEN']  # Length of input sequence
+INPUT_LEN = 96
 OUTPUT_LEN = regular_settings['OUTPUT_LEN']  # Length of output sequence
 TRAIN_VAL_TEST_RATIO = regular_settings['TRAIN_VAL_TEST_RATIO']  # Train/Validation/Test split ratios
 NORM_EACH_CHANNEL = regular_settings['NORM_EACH_CHANNEL'] # Whether to normalize each channel of the data
