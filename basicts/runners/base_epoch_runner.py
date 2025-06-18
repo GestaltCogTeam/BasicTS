zezhishao/GuangCang_BasicTS

feat: 🎸 add inference script
add experimental script for inference processing

basicts/runners/base_epoch_runner.py
@@ -251,8 +251,7 @@ class BaseEpochRunner(metaclass=ABCMeta):
         dataset = self.build_val_dataset(cfg)
         return build_data_loader(dataset, cfg['VAL.DATA'])
 
-    @staticmethod
-    def build_val_dataset(cfg: Dict) -> Dataset:
+    def build_val_dataset(self, cfg: Dict) -> Dataset:
         """It can be implement to build dataset for validation (not necessary).
 
         Args:
@@ -293,6 +292,22 @@ class BaseEpochRunner(metaclass=ABCMeta):
         """
 
         raise NotImplementedError('build_test_dataset method must be implemented.')
+
+    def build_inference_dataset(self, cfg: Dict, input_data: Union[str, list]) -> Dataset:
+        """
+        Build the inference dataset.
+
+        Args:
+            cfg (Dict): Configuration dictionary.
+
+        Returns:
+            Dataset: The inference dataset.
+
+        Raises:
+            NotImplementedError: Must be implemented in a subclass.
+        """
+
+        raise NotImplementedError('build_inference_dataset method must be implemented.')
 
     def init_training(self, cfg: Dict) -> None:
         """
@@ -371,6 +386,20 @@ class BaseEpochRunner(metaclass=ABCMeta):
         self.test_interval = cfg['TEST'].get('INTERVAL', 1)
         self.test_data_loader = self.build_test_data_loader(cfg)
         self.register_epoch_meter('test/time', 'test', '{:.2f} (s)', plt=False)
+
+    @master_only
+    def init_inference(self, cfg: Dict, input_data: Union[str, list]) -> None:
+        """
+        Initialize the inference data loader and related settings.
+
+        Args:
+            cfg (Dict): Configuration dictionary.
+            input_data (Union[str, list]): The input data file path or data list for inference.
+        """
+
+        self.inference_dataset = self.build_inference_dataset(cfg, input_data)
+        self.inference_dataset_loader = DataLoader(self.inference_dataset, batch_size=1, shuffle=False)
+        self.register_epoch_meter('inference/time', 'inference', '{:.2f} (s)', plt=False)
 
     # endregion Initialization Functions
 
@@ -532,6 +561,45 @@ class BaseEpochRunner(metaclass=ABCMeta):
 
         self.on_test_end()
 
+    @torch.no_grad()
+    @master_only
+    def inference_pipeline(self, cfg: Optional[Dict] = None, input_data: Union[str, list] = '', output_data_file_path: str = '') -> list:
+        """
+        The complete inference process.
+
+        Args:
+            cfg (Dict, optional): Configuration dictionary.
+            input_data (Union[str, list], optional): The input data file path or data list for inference.
+            output_data_file_path (str, optional): The output data file path. Defaults to '' meaning no output file.
+        """
+
+        # if isinstance(input_data, str):
+        #     pass
+
+        self.init_inference(cfg, input_data)
+
+        self.on_inference_start()
+
+        inference_start_time = time.time()
+        self.model.eval()
+
+        # execute the inference process
+        result = self.inference(save_result_path=output_data_file_path)
+
+        inference_end_time = time.time()
+        self.update_epoch_meter('inference/time', inference_end_time - inference_start_time)
+
+        self.print_epoch_meters('inference')
+
+        # logging here for intuitiveness
+        if output_data_file_path:
+            self.logger.info(f'inference results saved to {output_data_file_path}.')
+
+        self.on_inference_end()
+
+        return result
+
+
     # endregion Entries
 
     # region Main Loops
@@ -578,6 +646,19 @@ class BaseEpochRunner(metaclass=ABCMeta):
             train_epoch (int, optional): Current epoch during training. Defaults to None.
             save_metrics (bool, optional): Save the test metrics. Defaults to False.
             save_results (bool, optional): Save the test results. Defaults to False.
+
+        Raises:
+            NotImplementedError: Must be implemented in a subclass.
+        """
+
+        raise NotImplementedError('test method must be implemented.')
+
+    def inference(self, save_result_path: str = '') -> list:
+        """
+        Define the details of the inference process.
+
+        Args:
+            save_result_path (str, optional): The output data file path. Defaults to '' meaning no output file.
 
         Raises:
             NotImplementedError: Must be implemented in a subclass.
@@ -666,8 +747,20 @@ class BaseEpochRunner(metaclass=ABCMeta):
         pass
 
     @master_only
+    def on_inference_start(self) -> None:
+        """Callback at the start of inference."""
+
+        pass
+
+    @master_only
     def on_test_end(self) -> None:
         """Callback at the end of testing."""
+
+        pass
+
+    @master_only
+    def on_inference_end(self) -> None:
+        """Callback at the end of inference."""
 
         pass
 

basicts/runners/base_tsf_runner.py
@@ -11,6 +11,8 @@ from easydict import EasyDict
 from easytorch.utils import master_only
 from tqdm import tqdm
 
+from basicts.data.simple_inference_dataset import TimeSeriesInferenceDataset
+
 from ..metrics import (masked_mae, masked_mape, masked_mse, masked_rmse,
                        masked_wape)
 from .base_epoch_runner import BaseEpochRunner
@@ -253,6 +255,21 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
 
         return dataset
 
+    def build_inference_dataset(self, cfg: Dict, input_data: Union[str, list]):
+        """Build the inference dataset.
+
+        Args:
+            cfg (Dict): Configuration.
+            input_data (Union[str, list]): The input data file path or data list for inference.
+        Returns:
+            Dataset: The constructed inference dataset.
+        """
+
+        dataset = TimeSeriesInferenceDataset(dataset=input_data, logger=self.logger, **cfg['DATASET']['PARAM'])
+        self.logger.info(f'Test dataset length: {len(dataset)}')
+
+        return dataset
+
     def curriculum_learning(self, epoch: int = None) -> int:
         """Calculate task level for curriculum learning.
 
@@ -275,7 +292,7 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
             cl_length = min(progress, self.prediction_length)
         return cl_length
 
-    def forward(self, data: tuple, epoch: int = None, iter_num: int = None, train: bool = True, **kwargs) -> Dict:
+    def forward(self, data: tuple, epoch: Optional[int] = None, iter_num: Optional[int] = None, train: bool = True, **kwargs) -> Dict:
         """
         Performs the forward pass for training, validation, and testing. 
         Note: The outputs are not re-scaled.
@@ -444,6 +461,28 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
                 json.dump(metrics_results, f, indent=4)
 
         return returns_all
+
+    @torch.no_grad()
+    @master_only
+    def inference(self, save_result_path: str = '') -> list:
+        """Inference process.
+        
+        Args:
+            save_result_path (str): The path to save the inference results. Defaults to '' meaning no saving.
+        """
+
+        data = next(iter(self.inference_dataset_loader))
+
+        forward_return = self.forward(data, epoch=None, iter_num=None, train=False)
+        prediction = forward_return['prediction'].detach().cpu()
+        prediction = prediction.squeeze(0).squeeze(-1)
+
+        # save
+        if save_result_path:
+            # save prediction to save_result_path with csv format
+            np.savetxt(save_result_path, prediction, delimiter=',')
+
+        return prediction
 
     @master_only
     def on_validating_end(self, train_epoch: Optional[int]):

GestaltCogTeam/BasicTS

basicts/runners/base_epoch_runner.py
@@ -253,8 +253,7 @@ class BaseEpochRunner(metaclass=ABCMeta):
         dataset = self.build_val_dataset(cfg)
         return build_data_loader(dataset, cfg['VAL.DATA'])
 
-    @staticmethod
-    def build_val_dataset(cfg: Dict) -> Dataset:
+    def build_val_dataset(self, cfg: Dict) -> Dataset:
         """It can be implement to build dataset for validation (not necessary).
 
         Args:
@@ -295,6 +294,22 @@ class BaseEpochRunner(metaclass=ABCMeta):
         """
 
         raise NotImplementedError('build_test_dataset method must be implemented.')
+
+    def build_inference_dataset(self, cfg: Dict, input_data: Union[str, list]) -> Dataset:
+        """
+        Build the inference dataset.
+
+        Args:
+            cfg (Dict): Configuration dictionary.
+
+        Returns:
+            Dataset: The inference dataset.
+
+        Raises:
+            NotImplementedError: Must be implemented in a subclass.
+        """
+
+        raise NotImplementedError('build_inference_dataset method must be implemented.')
 
     def init_training(self, cfg: Dict) -> None:
         """
@@ -373,6 +388,20 @@ class BaseEpochRunner(metaclass=ABCMeta):
         self.test_interval = cfg['TEST'].get('INTERVAL', 1)
         self.test_data_loader = self.build_test_data_loader(cfg)
         self.register_epoch_meter('test/time', 'test', '{:.2f} (s)', plt=False)
+
+    @master_only
+    def init_inference(self, cfg: Dict, input_data: Union[str, list]) -> None:
+        """
+        Initialize the inference data loader and related settings.
+
+        Args:
+            cfg (Dict): Configuration dictionary.
+            input_data (Union[str, list]): The input data file path or data list for inference.
+        """
+
+        self.inference_dataset = self.build_inference_dataset(cfg, input_data)
+        self.inference_dataset_loader = DataLoader(self.inference_dataset, batch_size=1, shuffle=False)
+        self.register_epoch_meter('inference/time', 'inference', '{:.2f} (s)', plt=False)
 
     # endregion Initialization Functions
 
@@ -534,6 +563,47 @@ class BaseEpochRunner(metaclass=ABCMeta):
 
         self.on_test_end()
 
+    @torch.no_grad()
+    @master_only
+    # pylint: disable=unused-argument
+    def inference_pipeline(self, cfg: Optional[Dict] = None, input_data: Union[str, list] = '', output_data_file_path: str = '', **kwargs) -> tuple:
+        """
+        The complete inference process.
+
+        Args:
+            cfg (Dict, optional): Configuration dictionary.
+            input_data (Union[str, list], optional): The input data file path or data list for inference.
+            output_data_file_path (str, optional): The output data file path. Defaults to '' meaning no output file.
+            **kwargs: Additional keyword arguments for flexibility in inference.
+        """
+
+        # if isinstance(input_data, str):
+        #     pass
+
+        self.init_inference(cfg, input_data)
+
+        self.on_inference_start()
+
+        inference_start_time = time.time()
+        self.model.eval()
+
+        # execute the inference process
+        result = self.inference(save_result_path=output_data_file_path)
+
+        inference_end_time = time.time()
+        self.update_epoch_meter('inference/time', inference_end_time - inference_start_time)
+
+        self.print_epoch_meters('inference')
+
+        # logging here for intuitiveness
+        if output_data_file_path:
+            self.logger.info(f'inference results saved to {output_data_file_path}.')
+
+        self.on_inference_end()
+
+        return result
+
+
     # endregion Entries
 
     # region Main Loops
@@ -580,6 +650,19 @@ class BaseEpochRunner(metaclass=ABCMeta):
             train_epoch (int, optional): Current epoch during training. Defaults to None.
             save_metrics (bool, optional): Save the test metrics. Defaults to False.
             save_results (bool, optional): Save the test results. Defaults to False.
+
+        Raises:
+            NotImplementedError: Must be implemented in a subclass.
+        """
+
+        raise NotImplementedError('test method must be implemented.')
+
+    def inference(self, save_result_path: str = '') -> tuple:
+        """
+        Define the details of the inference process.
+
+        Args:
+            save_result_path (str, optional): The output data file path. Defaults to '' meaning no output file.
 
         Raises:
             NotImplementedError: Must be implemented in a subclass.
@@ -668,8 +751,20 @@ class BaseEpochRunner(metaclass=ABCMeta):
         pass
 
     @master_only
+    def on_inference_start(self) -> None:
+        """Callback at the start of inference."""
+
+        pass
+
+    @master_only
     def on_test_end(self) -> None:
         """Callback at the end of testing."""
+
+        pass
+
+    @master_only
+    def on_inference_end(self) -> None:
+        """Callback at the end of inference."""
 
         pass
 

basicts/runners/base_tsf_runner.py
@@ -11,6 +11,8 @@ from easydict import EasyDict
 from easytorch.utils import master_only
 from tqdm import tqdm
 
+from basicts.data.simple_inference_dataset import TimeSeriesInferenceDataset
+
 from ..metrics import (masked_mae, masked_mape, masked_mse, masked_rmse,
                        masked_wape)
 from .base_epoch_runner import BaseEpochRunner
@@ -253,6 +255,21 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
 
         return dataset
 
+    def build_inference_dataset(self, cfg: Dict, input_data: Union[str, list]):
+        """Build the inference dataset.
+
+        Args:
+            cfg (Dict): Configuration.
+            input_data (Union[str, list]): The input data file path or data list for inference.
+        Returns:
+            Dataset: The constructed inference dataset.
+        """
+
+        dataset = TimeSeriesInferenceDataset(dataset=input_data, logger=self.logger, **cfg['DATASET']['PARAM'])
+        self.logger.info(f'Inference dataset length: {len(dataset)}')
+
+        return dataset
+
     def curriculum_learning(self, epoch: int = None) -> int:
         """Calculate task level for curriculum learning.
 
@@ -275,7 +292,7 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
             cl_length = min(progress, self.prediction_length)
         return cl_length
 
-    def forward(self, data: tuple, epoch: int = None, iter_num: int = None, train: bool = True, **kwargs) -> Dict:
+    def forward(self, data: tuple, epoch: Optional[int] = None, iter_num: Optional[int] = None, train: bool = True, **kwargs) -> Dict:
         """
         Performs the forward pass for training, validation, and testing. 
         Note: The outputs are not re-scaled.
@@ -444,6 +461,47 @@ class BaseTimeSeriesForecastingRunner(BaseEpochRunner):
                 json.dump(metrics_results, f, indent=4)
 
         return returns_all
+
+    @torch.no_grad()
+    @master_only
+    def inference(self, save_result_path: str = '') -> tuple:
+        """Inference process.
+        
+        Args:
+            save_result_path (str): The path to save the inference results. Defaults to '' meaning no saving.
+        """
+
+        data = next(iter(self.inference_dataset_loader))
+
+        forward_return = self.forward(data, epoch=None, iter_num=None, train=False)
+        prediction = forward_return['prediction'].detach().cpu()
+        prediction = prediction.squeeze(0).squeeze(-1)
+
+        datetime_data = self._inference_get_data_time(prediction, self.inference_dataset.last_datetime, \
+                                                   self.inference_dataset.description['frequency (minutes)'])
+
+        # save
+        if save_result_path:
+            # save prediction to save_result_path with csv format
+            datetime_data = np.fromiter((x.astype('datetime64[us]').item().strftime('%Y-%m-%d %H:%M:%S')\
+                                         for x in datetime_data), 'S32').reshape(-1, 1)
+            save_data = np.concatenate((datetime_data, prediction.numpy().astype(str)), axis=1)
+            np.savetxt(save_result_path, save_data, delimiter=',', fmt='%s')
+
+        return prediction.numpy(), datetime_data
+
+    @torch.no_grad()
+    @master_only
+    def _inference_get_data_time(self, data: np.ndarray, last_datatime: np.datetime64, freq: int) -> np.ndarray:
+        """
+        Append new data to the existing data
+        """
+
+        datetime_data = np.arange(last_datatime + np.timedelta64(freq, 'm'),
+                             last_datatime + np.timedelta64(freq * (len(data) + 1), 'm'),
+                             np.timedelta64(freq, 'm'))
+
+        return datetime_data
 
     @master_only
     def on_validating_end(self, train_epoch: Optional[int]):