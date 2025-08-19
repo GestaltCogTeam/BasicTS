cd16eb27fef282ea196c815a0796b65dcf67057a
Yisong Fu <139831104+yisongfu@users.noreply.github.com>
2025-08-19 13:23:31 +0800

    Update __init__.py

--- basicts/utils/__init__.py
+++ basicts/utils/__init__.py
@@ -1,5 +1,6 @@
 from .config import get_dataset_name
 from .dataset import InfiniteGenerator
+from .meter_pool import MeterPool
 from .misc import check_nan_inf, clock
 from .misc import partial_func as partial
 from .misc import remove_nan_inf
@@ -12,4 +13,4 @@ __all__ = ['load_adj', 'load_pkl', 'dump_pkl',
            'remove_nan_inf', 'data_transformation_4_xformer',
            'partial', 'get_regular_settings',
            'load_dataset_data', 'load_dataset_desc',
-           'InfiniteGenerator', 'get_dataset_name']
+           'InfiniteGenerator', 'get_dataset_name', 'MeterPool']
