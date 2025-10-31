d247fe92637bcdbbc0c29c3293eb45b0b17522e2
DU YIFAN <duyifan@ict.ac.cn>
2025-10-31 17:22:40 +0800

    style: 💄 lint

--- tests/basicts_test/test_launcher.py
+++ tests/basicts_test/test_launcher.py
@@ -1,13 +1,2 @@
-import sys
-import os
-print(os.path.abspath(__file__ + "/../../../src/"))
-sys.path.append(os.path.abspath(__file__ + "/../../../src/"))
-os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-import types
-import importlib
-import traceback
-import pytest
-
 def test_launch():
     pass
