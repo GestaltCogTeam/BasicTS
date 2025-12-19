2cf1ebfd3039ccf33e6de9ec2e3bf3465a38d87d
yisongfu <jokerfulyfock@gmail.com>
2025-12-19 11:25:39 +0800

    update selective learning to support load configuration from json

--- src/basicts/runners/callback/__init__.py
+++ src/basicts/runners/callback/__init__.py
@@ -4,7 +4,6 @@ from .clip_grad import GradientClipping
 from .curriculum_learrning import CurriculumLearning
 from .early_stopping import EarlyStopping
 from .grad_accumulation import GradAccumulation
-from .koopa_mask_init import KoopaMaskInitCallbackFullTrain
 from .no_bp import NoBP
 from .selective_learning import SelectiveLearning
 
